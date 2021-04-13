9b08024cf8017906cc127bf33694c191a893aabd
Siegfred Samson <siegfredsamson@gmail.com>
2021-04-13 19:55:32 +0100

    deposit created

--- budgetapp.py
+++ budgetapp.py
@@ -4,3 +4,9 @@ class Shopping:
         self.cloth = clothing
         self.entertain = entertainment
 
+    # option = input("Enter food, clothing or entertainment")
+    balance = 0
+    def deposit(self):
+        input("W E L C O M E ! \n How Much would you like to deposit? \n")
+
+
