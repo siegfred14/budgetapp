637d2aa9336e3f7472c9b6eee9b917367210a570
Siegfred Samson <siegfredsamson@gmail.com>
2021-04-16 14:07:51 +0100

    menu method added

--- budgetapp.py
+++ budgetapp.py
@@ -1,61 +1,51 @@
-class Shopping:
+class Budget:
     def __init__(self, food, clothing, entertainment):
         self.food = food
         self.cloth = clothing
         self.entertain = entertainment
 
-    # creating a database for our budgets
-    budget_record = {
-        "food": 0,
-        "clothing": 0,
-        "entertainment": 0
-    }
+    # # creating a database for our budgets
+    # budget_record = {
+    #     "food": 0,
+    #     "clothing": 0,
+    #     "entertainment": 0
+    # }
+    #
+    # # setting values for keys
+    # set_food_budget = int(input("Enter Budget for food"))
+    # set_clothing_budget = int(input("Enter Budget for Clothing"))
+    # set_entertainment_budget = int(input("Enter Budget for Entertainment"))
+    #
+    # # updating database
+    # budget_record["food"] += set_food_budget
+    # budget_record["clothing"] += set_clothing_budget
+    # budget_record["entertainment"] += set_entertainment_budget
+    #
+    # # Total budget calculated
+    # total_budget = budget_record["food"] + budget_record["clothing"] + budget_record["entertainment"]
+    # print(f"Your Total Budget is {total_budget}")
+    #
 
-    # setting values for keys
-    set_food_budget = int(input("Enter Budget for food"))
-    set_clothing_budget = int(input("Enter Budget for Clothing"))
-    set_entertainment_budget = int(input("Enter Budget for Entertainment"))
-
-    # updating database
-    budget_record["food"] += set_food_budget
-    budget_record["clothing"] += set_clothing_budget
-    budget_record["food"] += set_entertainment_budget
-
-    # Total budget calculated
-    total_budget = budget_record["food"] + budget_record["clothing"] + budget_record["entertainment"]
-    print(f"Your Total Budget is {total_budget}")
-
-#
 #     item = {""}
 #     balance = 0
-#     def deposit(self):
-#         input("W E L C O M E ! \n How Much would you like to deposit? \n")
-#
-#
-#     def withdraw(self):
-#         balance = 0
-#         withdrawn = input("W E L C O M E ! \n How Much would you like to withdraw? \n")
-#         if balance >= withdrawn:
-#             balance -= withdrawn
-#             return balance
-#         else:
-#             print("insufficient funds!")
-#     def balance(self):
-#         pass
-#
-#     def transfer(self):
-#         transfer_from = input("Which Category would you like to transfer from? ... \n")
-#         transfer_amount = int(input("Enter Amount to transfer"))
-#
-#         transfer_to = int(input("Which Category would you like to transfer to? ...\n"))
-#
-#         # if transfer_from == food:
-#         #     food = food - transfer_amount
-#         #
-#         # else:
-#         #     print("Wrong Input")
-#
-#
-# shop = Shopping("pilau", "jacket", "movie")
-# print(shop.cloth)
-# shop.deposit()
+
+    def menu(self):
+        menu_choice = int(input("W E L C O M E ! \n Please pick an option \n 1.) Deposit \n 2.) Withdrawal \n 3.) "
+                                "Check Category Balances \n 4.) Transfer \n"))
+
+        if menu_choice == 1:
+            self.deposit()
+        elif menu_choice == 2:
+            self.withdraw()
+        elif menu_choice == 3:
+            self.balance()
+        elif menu_choice == 4:
+            self.transfer()
+        else:
+            print("Wrong input, Try again")
+            self.menu()
+
+
+shop = Budget(500, 4000, 3000)
+print(shop.cloth)
+shop.menu()
