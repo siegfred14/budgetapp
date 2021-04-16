96fe08a2310366f21933a24c56363f9dda0a1072
Siegfred Samson <siegfredsamson@gmail.com>
2021-04-16 15:21:39 +0100

    deposit method modified

--- budgetapp.py
+++ budgetapp.py
@@ -4,31 +4,6 @@ class Budget:
         self.cloth = clothing
         self.entertain = entertainment
 
-    # # creating a database for our budgets
-    # budget_record = {
-    #     "food": 0,
-    #     "clothing": 0,
-    #     "entertainment": 0
-    # }
-    #
-    # # setting values for keys
-    # set_food_budget = int(input("Enter Budget for food"))
-    # set_clothing_budget = int(input("Enter Budget for Clothing"))
-    # set_entertainment_budget = int(input("Enter Budget for Entertainment"))
-    #
-    # # updating database
-    # budget_record["food"] += set_food_budget
-    # budget_record["clothing"] += set_clothing_budget
-    # budget_record["entertainment"] += set_entertainment_budget
-    #
-    # # Total budget calculated
-    # total_budget = budget_record["food"] + budget_record["clothing"] + budget_record["entertainment"]
-    # print(f"Your Total Budget is {total_budget}")
-    #
-
-    #     item = {""}
-    #     balance = 0
-
     def menu(self):
         menu_choice = int(input("W E L C O M E ! \n Please pick an option \n 1.) Deposit \n 2.) Withdrawal \n 3.) "
                                 "Check Category Balances \n 4.) Transfer \n"))
@@ -45,6 +20,7 @@ class Budget:
             print("Wrong input, Try again")
             self.menu()
 
+    # Deposit method to add cash into a budget category
     def deposit(self):
         deposit_amount = int(input("W E L C O M E ! \n How Much would you like to deposit? \n "))
         deposit_choice = int(input('''
@@ -66,6 +42,7 @@ class Budget:
         else:
             print("Wrong input, Try again")
             self.deposit()
+        print("Transaction Successful!")
         self.balance()
 
     def withdraw(self):
@@ -140,4 +117,4 @@ class Budget:
 
 shop = Budget(500, 4000, 3000)
 print(shop.cloth)
-shop.transfer()
+shop.menu()
